bf2e68bf8cf969b1ac9cedc723d4cf090f2324cc
surya-0483 <sjagguma@gitam.in>
2026-03-17 13:38:24 +0530

    my_profile

---
+++ My_Profile.py
@@ -0,0 +1,2 @@
+print("Welcome to Codegnan")
+print("---------------------\nMy Name: Jaggumantri Surya Patnaik\nAge: 21 years\nBatch: CG-VSP-PFS-001\nI completed my B.Tech from Gitam University in 2025") 
